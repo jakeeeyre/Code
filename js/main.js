85b49d0aad90c021cad4705c5d3ef2a6c7d65912
hybridity <jakeeeyre@gmail.com>
2023-08-03 07:16:31 +0100

    updated all pages to be responsive

--- js/main.js
+++ js/main.js
@@ -41,57 +41,57 @@ const handleMediaQueryChange = (mediaQuery) => {
   mediaQuery.addEventListener('change', handleMediaQueryChange);
 
 // for carousel
-// const prevButton = document.querySelector('#left-button')
-// const nextButton = document.querySelector('#right-button')
-// const carousel = document.querySelector('.carousel')
-// const carouselTrack = document.querySelector('.carousel-contents')
-// const carouselSlides = Array.from(carouselTrack.children)
+const prevButton = document.querySelector('#left-button')
+const nextButton = document.querySelector('#right-button')
+const carousel = document.querySelector('.carousel')
+const carouselTrack = document.querySelector('.carousel-contents')
+const carouselSlides = Array.from(carouselTrack.children)
 
-// carouselSlides.forEach((slide, index) => {
+carouselSlides.forEach((slide, index) => {
 
-//     const widthOfSlide = carouselSlides[0].getBoundingClientRect().width
-//     slide.style.left = (widthOfSlide * index) + 'px'
+    const widthOfSlide = carouselSlides[0].getBoundingClientRect().width
+    slide.style.left = (widthOfSlide * index) + 'px'
 
-// })
+})
 
 
 
-// nextButton.addEventListener('click', () => {
-//     const currentSlide = carouselTrack.querySelector('.is-selected')
-//     const nextSlide = currentSlide.nextElementSibling
-//     const destination = nextSlide.style.left
-//     const currentDot = carousel.querySelector('.current-dot')
-//     const nextDot = currentDot.nextElementSibling
+nextButton.addEventListener('click', () => {
+    const currentSlide = carouselTrack.querySelector('.is-selected')
+    const nextSlide = currentSlide.nextElementSibling
+    const destination = nextSlide.style.left
+    const currentDot = carousel.querySelector('.current-dot')
+    const nextDot = currentDot.nextElementSibling
     
-//     carouselTrack.style.transform = 'translate(-' + destination + ')'
+    carouselTrack.style.transform = 'translate(-' + destination + ')'
 
-//     currentSlide.classList.remove('is-selected')
-//     nextSlide.classList.add('is-selected')
-//     currentDot.classList.remove('current-dot')
-//     nextDot.classList.add('current-dot')
-//     prevButton.style.opacity = 1
+    currentSlide.classList.remove('is-selected')
+    nextSlide.classList.add('is-selected')
+    currentDot.classList.remove('current-dot')
+    nextDot.classList.add('current-dot')
+    prevButton.style.opacity = 1
 
-//     if(!nextSlide.nextElementSibling) {
-//         nextButton.style.opacity = 0
-//     }
-// })
+    if(!nextSlide.nextElementSibling) {
+        nextButton.style.opacity = 0
+    }
+})
 
-// prevButton.addEventListener('click', () => {
-//     const currentSlide = carouselTrack.querySelector('.is-selected')
-//     const prevSlide = currentSlide.previousElementSibling
-//     const destination = prevSlide.style.left
-//     const currentDot = carousel.querySelector('.current-dot')
-//     const prevDot = currentDot.previousElementSibling
+prevButton.addEventListener('click', () => {
+    const currentSlide = carouselTrack.querySelector('.is-selected')
+    const prevSlide = currentSlide.previousElementSibling
+    const destination = prevSlide.style.left
+    const currentDot = carousel.querySelector('.current-dot')
+    const prevDot = currentDot.previousElementSibling
     
-//     carouselTrack.style.transform = 'translate(-' + destination + ')'
+    carouselTrack.style.transform = 'translate(-' + destination + ')'
 
-//     currentSlide.classList.remove('is-selected')
-//     prevSlide.classList.add('is-selected')
-//     currentDot.classList.remove('current-dot')
-//     prevDot.classList.add('current-dot')
-//     nextButton.style.opacity = 1
+    currentSlide.classList.remove('is-selected')
+    prevSlide.classList.add('is-selected')
+    currentDot.classList.remove('current-dot')
+    prevDot.classList.add('current-dot')
+    nextButton.style.opacity = 1
 
-//     if(!prevSlide.previousElementSibling) {
-//         prevButton.style.opacity = 0
-//     }
-// })
+    if(!prevSlide.previousElementSibling) {
+        prevButton.style.opacity = 0
+    }
+})
